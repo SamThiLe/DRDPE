0e8f681e9777553a6e2b1cda84eaf6be36403e1e
Justin Poirier <Poirier_Justin@hotmail.com>
2018-10-22 09:14:23 -0300

    Added Enum for ProductStatus

---
+++ DRDPE/Enumerations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DRDPE
+{
+    public enum ProductStatus
+    {
+        Available = 1,
+        Out_of_Stock = 2,
+        Back_Ordered = 3,
+        Temporarily_Available = 4,
+        Discontinued = 5
+    }
+}
