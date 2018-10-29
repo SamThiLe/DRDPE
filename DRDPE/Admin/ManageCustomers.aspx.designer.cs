3499b9e273ef21288d933199d5ef7baa5b5f0d9a
Keith Faulkner <wolfe.winters@gmail.com>
2018-10-29 14:34:25 -0300

    bgfnghfgdhd

--- DRDPE/Admin/ManageCustomers.aspx.designer.cs
+++ DRDPE/Admin/ManageCustomers.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace DRDPE.Admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSearch;
+        
+        /// <summary>
+        /// rptCust control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptCust;
     }
 }
