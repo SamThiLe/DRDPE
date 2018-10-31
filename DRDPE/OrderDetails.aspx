ec2a5c73258e6c5ae54d2e9422602ee24b407806
Justin Poirier <Poirier_Justin@hotmail.com>
2018-10-31 18:22:37 -0300

    Stuff

--- DRDPE/OrderDetails.aspx
+++ DRDPE/OrderDetails.aspx
@@ -2,4 +2,13 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h3>Your Order</h3>
+    <label runat="server" id="lblForOrderStatus">Order Status: </label>
+    <asp:Label runat="server" ID="lblOrderStatus"></asp:Label>
+    <br />
+    <label runat="server" id="lblForOrderDate">Order Placed On: </label>
+    <asp:Label runat="server" ID="lblOrderDate"></asp:Label>
+    <br />
+    <label runat="server" id="lblForOrderTotal">Order Total: $</label>
+    <asp:Label runat="server" ID="lblOrderTotal"></asp:Label>
 </asp:Content>
--- DRDPE/OrderDetails.aspx.cs
+++ DRDPE/OrderDetails.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,50 @@ namespace DRDPE
 {
     public partial class OrderDetails : System.Web.UI.Page
     {
+        private string cnnString = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+        private string authNumber = HttpContext.Current.Request.QueryString["auth"];
         protected void Page_Load(object sender, EventArgs e)
         {
+            GetOrderDetails();
+        }
 
+        private void GetOrderDetails()
+        {
+            SqlDataReader dr = default(SqlDataReader);
+            SqlCommand cmd = default(SqlCommand);
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(cnnString))
+                {
+                    cmd = new SqlCommand("getOrderDetails", conn);
+                    cmd.Parameters.Add("@authNumber", SqlDbType.NVarChar, 50).Value = authNumber;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    dr = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    while (dr.Read())
+                    {
+                        lblOrderStatus.Text = ((OrderStatus)(Convert.ToInt32(dr["orderStatus"]))).ToString();
+                        lblOrderDate.Text = Convert.ToDateTime(dr["orderDate"]).ToString("dd/MM/yyyy");
+                    }
+                    dr.Close();
+                    cmd = new SqlCommand("getOrderTotal", conn);
+                    cmd.Parameters.Add("@authNumber", SqlDbType.NVarChar, 50).Value = authNumber;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    while (dr.Read())
+                    {
+                        lblOrderTotal.Text = dr["GrandTotal"].ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                dr.Close();
+            }
         }
     }
 }
